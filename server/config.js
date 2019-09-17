283ebe73c92b00aaf0d86668bc06d45f8b8d5b86
David Stone <davidjamesstone@gmail.com>
2019-09-17 14:23:02 +0100

    Update dependencies (#51)
    * Update dependencies
    
    * Add start task

--- server/config.js
+++ server/config.js
@@ -1,10 +1,11 @@
 const joi = require('@hapi/joi')
+const envs = ['development', 'test', 'production']
 
 // Define config schema
-const schema = {
+const schema = joi.object().keys({
   port: joi.number().default(3000),
-  env: joi.string().valid('development', 'test', 'production').default('development')
-}
+  env: joi.string().valid(...envs).default(envs[0])
+})
 
 // Build config
 const config = {
@@ -13,20 +14,11 @@ const config = {
 }
 
 // Validate config
-const result = joi.validate(config, schema, {
-  abortEarly: false
-})
+const { error, value } = schema.validate(config)
 
 // Throw if config is invalid
-if (result.error) {
-  throw new Error(`The server config is invalid. ${result.error.message}`)
+if (error) {
+  throw new Error(`The server config is invalid. ${error.message}`)
 }
-
-// Use the joi validated value
-const value = result.value
-
-// Add some helper props
-value.isDev = value.env === 'development'
-value.isProd = value.env === 'production'
 
 module.exports = value
